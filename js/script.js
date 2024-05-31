75ebc120e4dd3fc7621440a04f7a5ee89a781ca7
Phil Hempel <138188162+ZyberPhil@users.noreply.github.com>
2024-05-31 18:05:14 +0200

    First upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let currentIndex = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slides img').length;
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
